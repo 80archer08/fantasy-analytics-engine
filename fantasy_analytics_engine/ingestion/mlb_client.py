7f65d3393d01b8cc40b2d7a29b2100c31a5a417d
Giovanni Felix <giovanni.r.felix@gmail.com>
2026-03-02 17:06:05 -0500

    Initialize ingestion layer

--- fantasy_analytics_engine/ingestion/mlb_client.py
+++ fantasy_analytics_engine/ingestion/mlb_client.py
@@ -3,3 +3,12 @@
 Planned implementation target: MLB-StatsAPI package.
 https://pypi.org/project/MLB-StatsAPI/
 """
+
+from datetime import date
+from collections.abc import Iterable
+from fantasy_analytics_engine.domain.models import HitterGameStat, PitcherGameStat
+
+class MLBStatsClientProtocol:
+    def fetch_hitter_game_stats(self, start_date: date, end_date: date) -> Iterable[HitterGameStat]: ...
+    def fetch_pitcher_game_stats(self, start_date: date, end_date: date) -> Iterable[PitcherGameStat]: ...
+    
