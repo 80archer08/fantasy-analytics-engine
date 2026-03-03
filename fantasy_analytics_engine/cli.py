678a1310c372c8391701de61840ff8bc4b4f9b51
Giovanni Felix <giovanni.r.felix@gmail.com>
2026-03-02 21:04:56 -0500

    Updated commands

--- fantasy_analytics_engine/cli.py
+++ fantasy_analytics_engine/cli.py
@@ -1,2 +1,5 @@
 """TODO: implement module."""
-fae rank --start 2025-03-30 --end 2025-10-01 --league-size 4 --out ./output/
+fae ingest --start 2025-03-30 --end 2025-10-01
+fae score --start 2025-03-30 --end 2025-10-01
+fae replacement --start 2025-03-30 --end 2025-10-01 --league-size 4
+fae rank --as-of 2026-03-2 --league-size 4 --out ./output/
